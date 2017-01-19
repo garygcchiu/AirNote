dece5d51519e4d3fa6e4809e303ecd113afdbf1f
Gary <gary.gc.chiu@gmail.com>
2017-01-18 21:08:29 -0500

    implemented notes functionality

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,15 +1,29 @@
 class NotesController < ApplicationController
 
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
+
   def index
+    @notes = Note.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+
+    @note = Note.new(note_params)
+
+    #redirect to the note after submitting
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
+
   end
 
   def edit
@@ -20,10 +34,13 @@ class NotesController < ApplicationController
 
   private
 
+  # method willbe used for previous methods to find which note
   def find_note
+    @note = Note.find(params[:id])
   end
 
   def note_params
+    params.require(:note).permit(:title, :content)
   end
 
 end
